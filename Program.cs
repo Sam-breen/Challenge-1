44cb9a076b0487b0422ba35a3f1b9c3ee8a9ac67
Sam-breen <79820143+Sam-breen@users.noreply.github.com>
2021-05-06 17:09:14 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Challenge
+{
+    class Program
+    {
+        static void diceroll()
+        {   
+            Random rd = new Random();
+            int randomNumber = rd.Next(1,7);
+            Console.WriteLine(randomNumber);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to roll the dice?");
+            string answer = Console.ReadLine();
+            int counter = 0;
+
+            if (answer == "yes")
+            {
+                diceroll();
+                counter = counter + 1;
+
+                Console.WriteLine("Do you want to roll the dice?");
+                string answer2 = Console.ReadLine();
+
+                if (answer2 == "yes") 
+                {
+                    diceroll();
+                    counter = counter + 1;
+                }
+
+            }
+
+            else if (answer == "no") 
+            {
+                Console.WriteLine("Goodbye");
+                Environment.Exit(1);
+            }
+
+
+            int[] diceCount = new int[]{counter};
+            Console.WriteLine("dice rolls total = " + counter);
+
+           
+        }
+    }
+}
